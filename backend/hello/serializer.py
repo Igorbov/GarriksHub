70337a97fbfd29ad0c2aad86c62c6a619f46d97d
Igorbov <garri-g@bk.ru>
2024-04-16 00:05:51 +0300

    Readme_ud
    Readme file was updated

--- backend/hello/serializer.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class NewsSerializer(serializers.Serializer):
-    link = serializers.CharField(max_length=50)
-    rate = serializers.IntegerField()
-    # author = serializers.CharField(source='author.username', max_length=200)
